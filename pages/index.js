0a62ac038fe26fdafb4b87db91692110ece7bc24
Liz <liz.montesano@gmail.com>
2023-08-27 12:55:34 -0400

    removing if error/!data to maybe fix react bug

--- pages/index.js
+++ pages/index.js
@@ -10,9 +10,9 @@ export default function Index() {
   //There are 3 possible states: (1) loading when data is null (2) ready when the data is returned (3) error when there was an error fetching the data
   const { data, error } = useSWR("/api/staticdata", fetcher);
   //Handle the error state
-  if (error) return <div>Failed to load</div>;
+  //if (error) return <div>Failed to load</div>;
   //Handle the loading state
-  if (!data) return <div>Loading...</div>;
+  //if (!data) return <div>Loading...</div>;
   //Handle the ready state and display the result contained in the data object mapped to the structure of the json file
   
   //Sorting state and logic
