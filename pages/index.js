f96f3ce8bdd741e7479660c915a7b806592c6d86
Liz <liz.montesano@gmail.com>
2023-08-27 20:57:39 -0400

    rolling back

--- pages/index.js
+++ pages/index.js
@@ -61,7 +61,7 @@ export default function Index() {
             <p>Price: {item.price}</p>
             <p>Shipping: {item.delivery}</p>
             <p>Returns: {item.returns}</p>
-            <a style="color: blue" href={item.listing_url}>View Listing</a>
+            <a href={item.listing_url}>View Listing</a>
             <p>Source: {item.source}</p>
           </li>
         ))}
