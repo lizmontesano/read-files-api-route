7e757f6357c61224dd593e647aa0aca70c442a10
Liz <liz.montesano@gmail.com>
2023-08-29 11:32:28 -0400

    hiding results until selection is made

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Index() {
   //Selecting data, sorting by price, filtering by source
   const [sortBy, setSortBy] = useState('None'); // Default sorting by price
   const [selectedSource, setSelectedSource] = useState('All'); // Default: show all items
-  const [selectedData, setSelectedData] = useState('defaultdata'); // Default: use data1.json
+  const [selectedData, setSelectedData] = useState('oculusdata'); // Default: use data1.json
 
   //Set up SWR to run the fetcher function when calling "/api/staticdata"
   //There are 3 possible states: (1) loading when data is null (2) ready when the data is returned (3) error when there was an error fetching the data
@@ -34,6 +34,8 @@ export default function Index() {
     combinedData.sort((a, b) => parseFloat(b.price.replace(/[$,]/g, '')) - parseFloat(a.price.replace(/[$,]/g, '')));
   }
 
+  const isResultsVisible = selectedData !== 'none'; // Check if a data source is selected
+
   return (    
     <div>
       <style>
@@ -48,11 +50,12 @@ export default function Index() {
       <div>
         <label>Select Data:</label>
         <select onChange={(e) => setSelectedData(e.target.value)}>
-          <option value="defaultdata"> ... </option>
+          <option value="none">Select</option>
           <option value="oculusdata">Oculus</option>
           <option value="chromechairdata">Chrome Chair</option>
         </select>
       </div>
+      {isResultsVisible && (
       <div>
         <h2>Showing search results...</h2>
         <div>
@@ -86,6 +89,7 @@ export default function Index() {
         ))}
       </ul>
       </div>
+      )}
     </div>
   );
 }
