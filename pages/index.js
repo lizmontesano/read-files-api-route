abea78d1500f953ec2af858265d7ae707aab3e54
Liz <liz.montesano@gmail.com>
2023-09-07 12:23:43 -0400

    duplicate options

--- pages/index.js
+++ pages/index.js
@@ -163,7 +163,6 @@ export default function Index() {
             <option value="Free local pick up from Forest Hills, NY">Pickup in Forest Hills, NY</option>
             <option value="Free local pick up from Inwood, NY">Pickup in Inwood, NY</option>
             <option value="Free local pick up from West Nyack, NY">Pickup in West Nyack, NY</option>
-            <option value="Free local pick up from Inwood, NY">Pickup in Inwood, NY</option>
             <option value="Shipping">Shipping</option>
           </select>
       </div>
